bc425b3793655fead659ed4968966262bc7acb15
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-12-16 09:02:55 -0300

    feat: add multer storage method

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { uploadFile } = require("../controllers/uploadFile");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const router = express.Router();
 
-router.post("/upload", upload.single("file"), uploadFile);
+router.get("/upload", upload.single("file"), uploadFile);
 
 module.exports = router;
