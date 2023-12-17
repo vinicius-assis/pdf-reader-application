ad7046fa66463e6a3b10a56569d3ed99bfbb07ad
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-12-17 12:00:30 -0300

    feat: add multer configuration to tmp file

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const { uploadFile } = require("../controllers/uploadFile");
 const multer = require("multer");
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: `${__dirname}/../uploads/tmp-file`,
+  filename: function (req, file, cb) {
+    cb(null, "tmpFile.pdf");
+  },
+});
+
 const upload = multer({ storage: storage });
 
 const router = express.Router();
