8a63d176c82acf343b0fdda2cc6b007356a3f230
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-12-17 12:01:11 -0300

    feat: add functionality to remove tmp file after convert it

--- controllers/uploadFile.js
+++ controllers/uploadFile.js
@@ -1,13 +1,18 @@
+const fs = require("fs");
 const { Poppler } = require("pdf-images");
 
 const uploadFile = async (req, res) => {
   try {
+    const tmpFilePath = `${__dirname}/../uploads/tmp-file/tmpFile.pdf`;
     const result = Poppler.convert(
-      `${__dirname}/../example/test.pdf`,
+      tmpFilePath,
       `${__dirname}/../uploads`,
       "result"
     );
-    console.log(result);
+
+    if (result.success) {
+      fs.rm(tmpFilePath, () => console.log("Tmp file removed"));
+    }
     res.status(200).json({
       status: "Success",
       message: "Deu bom",
